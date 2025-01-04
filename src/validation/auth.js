3b373811df31b70232aa0b6988d8509ba7efce03
xPhoenixUAx <pavloff12@gmail.com>
2025-01-04 18:43:18 +0200

    upd

--- src/validation/auth.js
+++ src/validation/auth.js
@@ -1,12 +1,12 @@
 import Joi from "joi";
 
 export const registerSchema = Joi.object({
-  name: Joi.string().min(3).max(20).required(),
-  email: Joi.string().min(3).max(20).email().required(),
-  password: Joi.string().min(6).max(20).required(),
+  name: Joi.string().min(3).required(),
+  email: Joi.string().min(3).email().required(),
+  password: Joi.string().min(6).required(),
 });
 
 export const loginSchema = Joi.object({
-  email: Joi.string().min(3).max(20).email().required(),
-  password: Joi.string().min(6).max(20).required(),
+  email: Joi.string().min(3).email().required(),
+  password: Joi.string().min(6).required(),
 });
